a6f6cb56ed0e12f7b7d5c783f4751d5b2068b07b
Isuru Fernando <isuruf@gmail.com>
2018-07-07 20:41:16 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export LDFLAGS="$LDFLAGS -Wl,-rpath,${PREFIX}/lib"
+# There's a ._qd-${PKG_VERSION} file in the root folder along with qd-${PKG_VERSION}
+cd "qd-${PKG_VERSION}"
 
 ./configure --prefix=${PREFIX} --enable-shared
 
