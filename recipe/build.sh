5569ddb5def4b564af1a49de14c710baae3cb8eb
Isuru Fernando <isuruf@gmail.com>
2019-10-30 18:26:01 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,-dead_strip_dylibs//g")
+export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,--as-needed//g")
 
-./configure --prefix=${PREFIX} --enable-shared
+./configure --prefix=${PREFIX} --enable-shared || (cat config.log; false)
 
-make
+make -j${CPU_COUNT}
 make check
 make install
