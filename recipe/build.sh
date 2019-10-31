4eb0f02c517578ec05cdc2a14ee92cdaeb917dd3
Isuru Fernando <isuruf@gmail.com>
2019-10-30 19:16:20 -0500

    reconf

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@
 export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,-dead_strip_dylibs//g")
 export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,--as-needed//g")
 
+autoreconf -i
+chmod +x configure
+
 ./configure --prefix=${PREFIX} --enable-shared --host=$HOST || (cat config.log; false)
 
 make -j${CPU_COUNT}
