21332328a964413799f5c64a89782cc6672b6c16
Isuru Fernando <isuruf@gmail.com>
2019-10-30 19:02:06 -0500

    set host

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,-dead_strip_dylibs//g")
 export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,--as-needed//g")
 
-./configure --prefix=${PREFIX} --enable-shared || (cat config.log; false)
+./configure --prefix=${PREFIX} --enable-shared --host=$HOST || (cat config.log; false)
 
 make -j${CPU_COUNT}
 make check
